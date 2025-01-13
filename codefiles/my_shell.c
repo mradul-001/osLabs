ccfb4bfc9a508b0b3ae8361391335e3409490fe5
mradul-001 <sonkarmradul@gmail.com>
2025-01-13 17:00:13 +0530

    Implemented basic shell functionality, specifically the A part of the problem

--- codefiles/my_shell.c
+++ codefiles/my_shell.c
@@ -1,75 +1,91 @@
-#include  <stdio.h>
-#include  <sys/types.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
 
-/* Splits the string by space and returns the array of tokens
-*
-*/
-char **tokenize(char *line)
-{
+// Splits the string by space and returns the array of tokens
+char **tokenize(char *line) {
   char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
   char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
   int i, tokenIndex = 0, tokenNo = 0;
 
-  for(i =0; i < strlen(line); i++){
+  for (i = 0; i < strlen(line); i++) {
 
     char readChar = line[i];
 
-    if (readChar == ' ' || readChar == '\n' || readChar == '\t'){
+    if (readChar == ' ' || readChar == '\n' || readChar == '\t') {
       token[tokenIndex] = '\0';
-      if (tokenIndex != 0){
-	tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
-	strcpy(tokens[tokenNo++], token);
-	tokenIndex = 0; 
+      if (tokenIndex != 0) {
+        tokens[tokenNo] = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+        strcpy(tokens[tokenNo++], token);
+        tokenIndex = 0;
       }
     } else {
       token[tokenIndex++] = readChar;
     }
   }
- 
+
   free(token);
-  tokens[tokenNo] = NULL ;
+  tokens[tokenNo] = NULL; // null terminating the token array
   return tokens;
 }
 
+int main(int argc, char *argv[]) {
+  
+  char line[MAX_INPUT_SIZE];
+  char **tokens;
+  int i;
 
-int main(int argc, char* argv[]) {
-	char  line[MAX_INPUT_SIZE];            
-	char  **tokens;              
-	int i;
+  while (1) {
 
+    /* BEGIN: TAKING INPUT */
+    bzero(line, sizeof(line));
+    printf("$ ");
+    scanf("%[^\n]", line);
+    getchar();
+    /* END: TAKING INPUT */
 
-	while(1) {			
-		/* BEGIN: TAKING INPUT */
-		bzero(line, sizeof(line));
-		printf("$ ");
-		scanf("%[^\n]", line);
-		getchar();
+    line[strlen(line)] = '\n'; // terminate with new line
+    tokens = tokenize(line);
 
-		printf("Command entered: %s (remove this debug output later)\n", line);
-		/* END: TAKING INPUT */
+    // if the command is "cd"
+    if (strcmp(tokens[0], "cd") == 0) {
+      int chDir = chdir(tokens[1]);
+      if (chDir == 0) {
+        char *cwd = getcwd(NULL, 0);
+        printf("Current directory: %s\n", cwd);
+      } else {
+        printf("No such directory!\n");
+      }
 
-		line[strlen(line)] = '\n'; //terminate with new line
-		tokens = tokenize(line);
-   
-       //do whatever you want with the commands, here we just print them
+    }
+    else {
+      // forking a child process to run the command
+      pid_t cpid = fork();
+      if (cpid < 0) {
+        printf("Something wrong occurred!\n");
+      } else if (cpid == 0) {
+        // we have "tokens" as the argument array
+        int isError = execvp(tokens[0], tokens);
+        if (isError) {
+          printf("No such command found!\n");
+        }
+      } else {
+        wait(NULL);
+      }
+    }
 
-		for(i=0;tokens[i]!=NULL;i++){
-			printf("found token %s (remove this debug output later)\n", tokens[i]);
-		}
-       
-		// Freeing the allocated memory	
-		for(i=0;tokens[i]!=NULL;i++){
-			free(tokens[i]);
-		}
-		free(tokens);
-
-	}
-	return 0;
+    // Freeing the allocated memory
+    for (i = 0; tokens[i] != NULL; i++) {
+      free(tokens[i]);
+    }
+    free(tokens);
+  }
+  return 0;
 }
